02835ee3f50df1aa4fcc3b452168df6439c076cc
Maly Orlinsky <Mo216175919@sgur.org.il>
2026-05-13 19:13:20 +0300

    end

---
+++ Homework2/eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: { ecmaVersion: 2021 },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
